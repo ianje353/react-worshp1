e23b4370ad9b5273142f087030fe7dfe98c19f5b
ianje353 <i.nierre.528223@umidanao.edu.ph>
2025-02-21 16:57:28 +0800

    push para sa workshop

---
+++ src/components/clickme.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Button } from '@/components/ui/button'
+const clickme = () => {
+  return (
+    <div>
+      <Button>Click Me</Button>
+    </div>
+  )
+}
+
+export default clickme
